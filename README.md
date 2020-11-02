5ca8b4df94b1b2dd2f723cc855914d67ed779719
Ayoub Elaich <ayoub.elaich.93@gmail.com>
2020-11-01 19:17:06 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Summary
+
+This is a tool that is actually very good at using Amass command to do a good recon for a wide scope target
+
+## Requirement
+
+This tools requires [Amass](https://github.com/OWASP/Amass) and [Metabigor](https://github.com/J3ssie/metabigor)
+
+## How this tool works ?
+
+This tools is an automation that I did is based on the workflow that [Haklule](https://github.com/hakluke) showed in [Blog](https://medium.com/@hakluke/haklukes-guide-to-amass-how-to-use-amass-more-effectively-for-bug-bounties-7c37570b83f7)
+
+## Usage:
+
+./reconizer $(ORG name)
---
+++ reconizer.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+END='\033[0m'
+
+QUOTES=(
+        "Activating 1337 mode!"
+        "Target uses Equifax-grade security."
+        "ᕕ( ᐛ )ᕗ"
+        "ᕕ( ᐕ )ᕗ"
+        "三三ᕕ( ᐛ )ᕗ"
+        "ᐠ( ᐛ )ᐟ"
+        "Never gonna give you up."
+        "Js pls."
+        "Update pls."
+        "Sleep is for the weak."
+        "Grab a cuppa!"
+        "js, js+ on steroids."
+        "I am 100 percent natural."
+        "A bug is never just a mistake. It represents something bigger. An error of thinking that makes you who you are."
+        "You hack people. I hack time."
+        "I hope you don't screw like you type."
+        "Hack the planet!"
+        "Crypto stands for cryptography."
+        "PoC||GTFO"
+)
+
+rand=$((RANDOM % ${#QUOTES[@]}))
+printf "${YELLOW}[i]${END} ${QUOTES[$rand]}\n"
+echo
+
+ORG=$1
+
+getrange () {
+    printf "${GREEN}[+]${END} Getting IP Range of $ORG\n"
+    cat "$ORG-ASN.txt" | metabigor net --asn | tee -a $ORG-range.txt
+}
+
+getasn () {
+    printf "${GREEN}[+]${END} Getting ASN of $ORG\n"
+    amass intel -org "$ORG" | awk -F ',' '{print $1}' | tee -a $ORG-ASN.txt
+}
+
+getcert () {
+    printf "${GREEN}[+]${END} Getting Cert for $ORG\n"
+    for range in $(cat $ORG-range.txt); do amass intel -active -cidr $range;done | tee -a $ORG-domains2.txt
+}
+
+getcert2(){
+    printf "${GREEN}[+]${END} Getting V2 Cert for $ORG\n"
+    for asn in $(cat $ORG-ASN.txt);do amass intel -active -asn $asn;done | tee -a $ORG-domains.txt
+}
+
+getdomains(){
+    printf "${GREEN}[+]${END} Getting domains from all\n"
+    amass enum -dL $ORG-domains.txt -config ~/opt/config.ini
+    amass enum -dL $ORG-domains2.txt -config ~/opt/config.ini
+    printf "${YELLOW}[+]${END} Script is done.\n"
+}
+getasn
+getrange
+getcert
+getcert2
+getdomains
